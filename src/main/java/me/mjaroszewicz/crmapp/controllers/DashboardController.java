0fa9e89c1aa2a8c16d9a4c171bbb8e0312ebe839
Michal <238181@student.pwr.edu.pl>
2018-01-21 20:58:24 +0100

    Dashboard

--- src/main/java/me/mjaroszewicz/crmapp/controllers/DashboardController.java
+++ src/main/java/me/mjaroszewicz/crmapp/controllers/DashboardController.java
@@ -1,5 +1,7 @@
 package me.mjaroszewicz.crmapp.controllers;
 
+import me.mjaroszewicz.crmapp.services.DataAggregationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +11,19 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/dashboard")
 public class DashboardController {
 
+    @Autowired
+    private DataAggregationService dataAggregationService;
+
     @GetMapping
     public ModelAndView getDashboard(ModelAndView mv){
 
         mv.setViewName("dashboard");
+
+        mv.addObject("clientsCount", dataAggregationService.getClientsCount());
+        mv.addObject("activeOrdersCount", dataAggregationService.getActiveOrdersCount());
+        mv.addObject("completedOrdersCount", dataAggregationService.getCompletedOrdersCount());
+        mv.addObject("unresolvedComplaintsCount", dataAggregationService.getUnresolvedComplaintsCount());
+        mv.addObject("ordersValueSum", dataAggregationService.getOrdersValueSum());
 
 
         return mv;
---
+++ src/main/java/me/mjaroszewicz/crmapp/services/DataAggregationService.java
@@ -0,0 +1,71 @@
+package me.mjaroszewicz.crmapp.services;
+
+import me.mjaroszewicz.crmapp.entities.Complaint;
+import me.mjaroszewicz.crmapp.entities.Order;
+import me.mjaroszewicz.crmapp.repositories.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DataAggregationService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ComplaintRepository complaintRepository;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private PaymentRepository paymentRepository;
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    public long getClientsCount(){
+        return clientRepository.count();
+    }
+
+    public long getActiveOrdersCount(){
+        long count = 0;
+        for(Order o: orderRepository.findAll()){
+            if(o.getState() < 1)
+                count++;
+        }
+
+        return count;
+    }
+
+    public long getCompletedOrdersCount(){
+        long count = 0;
+        for(Order o: orderRepository.findAll()){
+            if(o.getState() == 1)
+                count++;
+        }
+        return count;
+    }
+
+    public long getUnresolvedComplaintsCount(){
+        long count = 0;
+        for(Complaint c: complaintRepository.findAll()){
+            if(c.getState() != 1)
+                count++;
+        }
+
+        return count;
+    }
+
+    public double getOrdersValueSum(){
+        double sum = 0;
+        for(Order o: orderRepository.findAll())
+            sum += o.getValue();
+        return sum;
+
+    }
+
+
+
+
+}
