2a21e51955adcdab89c9f1f5b415f233172b9dd7
Michal <238181@student.pwr.edu.pl>
2018-01-21 13:00:42 +0100

    Enabled security, added login page

---
+++ src/main/java/me/mjaroszewicz/crmapp/controllers/LoginController.java
@@ -0,0 +1,15 @@
+package me.mjaroszewicz.crmapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String getLoginPage(){
+
+        return "login";
+    }
+
+}
