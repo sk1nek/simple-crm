55a63d980ed41ba7adb6c9bcdc58a0b9e7c77bbd
Michal <238181@student.pwr.edu.pl>
2018-01-30 12:08:26 +0100

    Added notification modules

--- src/main/java/me/mjaroszewicz/crmapp/controllers/DashboardController.java
+++ src/main/java/me/mjaroszewicz/crmapp/controllers/DashboardController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Arrays;
+
 @Controller
 @RequestMapping("/dashboard")
 public class DashboardController {
