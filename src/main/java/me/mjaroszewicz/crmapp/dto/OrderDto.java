39298f5ec06937ada5695209339c9bff8c099950
Michal <238181@student.pwr.edu.pl>
2018-02-04 19:59:52 +0100

    Quick validation fix

--- src/main/java/me/mjaroszewicz/crmapp/dto/OrderDto.java
+++ src/main/java/me/mjaroszewicz/crmapp/dto/OrderDto.java
@@ -3,6 +3,9 @@ package me.mjaroszewicz.crmapp.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotNull;
 
 @Data
 @AllArgsConstructor
@@ -11,11 +14,15 @@ public class OrderDto {
 
     private Long clientId;
 
+    @NotNull
+    @Length(min = 3, max = 100)
     private String orderName;
 
     private String description;
 
+    @NotNull
     private double value;
 
+    @NotNull
     private String dateDeadline;
 }
